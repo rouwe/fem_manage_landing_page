1d190dea58460eaaac566c579e5417830a54718d
rouwe <santosroweme14@gmail.com>
2022-01-24 12:04:34 +0800

    Mobile features section interface

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@
     :element bgOverlay: .menu-bg-overlay - background overlay
     :element heroImg: .hero-img - background image to hide
 */
+const headerNavBox = document.getElementsByClassName('header-nav-box')[0];
 const headerNav = document.getElementsByClassName('header-nav')[0];
 const navOpen = document.getElementsByClassName('header-nav-open')[0];
 const navClose = document.getElementsByClassName('header-nav-close')[0];
@@ -12,6 +13,7 @@ const bgOverlay = document.getElementsByClassName('menu-bg-overlay')[0];
 const heroImg = document.getElementsByClassName('hero-img')[0];
 navOpen.addEventListener("click", () => {
     // Opens menu
+    headerNavBox.style.visibility = 'visible';
     headerNav.style.transform = 'translateX(0rem)';
     navOpen.style.display = 'none';
     navClose.style.display = 'block';
@@ -20,6 +22,7 @@ navOpen.addEventListener("click", () => {
 })
 navClose.addEventListener("click", () => {
     // Closes menu
+    headerNavBox.style.visibility = 'hidden';
     headerNav.style.transform = 'translateX(1000rem)';
     navOpen.style.display = 'inline';
     navClose.style.display = 'none';
